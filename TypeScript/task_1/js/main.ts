19b3f957aa19716ff2633273c3e0ced5d486ff50
Gromuf <luigi.beaumois@gmail.com>
2025-01-27 15:38:46 +0100

    task 3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -31,3 +31,14 @@ const director1: Directors = {
 	numberOfReports: 17,
   };
   console.log(director1);
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+	const x = firstName.charAt(0).toUpperCase();
+	return `${x}. ${lastName}`;
+}
+
+console.log(printTeacher("Jhon", "Doe"));

6441a5a61d39536a5dd8eb396ab003a47c9424f4
Gromuf <luigi.beaumois@gmail.com>
2026-01-19 14:26:24 +0100

    refactor: remove unnecessary document.ready wrapper in index.js

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,8 +1,6 @@
 //index.js
 const $ = require("jquery");
 
-$(document).ready(() => {
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<p>Copyright - Holberton School</p>");
-});
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");

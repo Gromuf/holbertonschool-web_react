8bc93a702aa9063f4908a5acee01bbf59e189ffa
Gromuf <luigi.beaumois@gmail.com>
2026-01-21 10:32:10 +0100

    fix: upgrade jQuery to version 4.0.0 and update import syntax in index.js

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,6 +1,7 @@
-//index.js
-import $ from "jquery";
+const $ = require("jquery");
 
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+$(document).ready(function () {
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<p>Copyright - Holberton School</p>");
+});

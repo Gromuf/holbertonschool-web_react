ac4f3171ce77f7f47a91d904bb19d6736da2c691
Gromuf <luigi.beaumois@gmail.com>
2026-01-21 11:05:28 +0100

    fix: update text in dashboard to clarify data context for students

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -8,7 +8,7 @@ function updateCounter() {
 }
 $(function() {
   $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard for the students</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
   $('body').append(`<p id='count'></p>`);
   $('body').append('<p>Copyright - Holberton School</p>');

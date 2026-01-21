0b46b10a2ac59a4a18727326e054d8f331018ea1
Gromuf <luigi.beaumois@gmail.com>
2026-01-21 10:56:09 +0100

    fix: wrap DOM manipulation in jQuery ready function for better loading behavior

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -2,15 +2,15 @@ const $ = require('jquery');
 const _ = require('lodash');
 
 let count = 0;
-
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append(`<p id='count'></p>`);
-$('body').append('<p>Copyright - Holberton School</p>');
-
 function updateCounter() {
   count += 1;
   $('#count').text(`${count} clicks on the button`);
 }
-$('button').on('click', _.debounce(updateCounter, 500));
+$(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append(`<p id='count'></p>`);
+  $('body').append('<p>Copyright - Holberton School</p>');
+  $('button').on('click', _.debounce(updateCounter, 500));
+});

2d670d5f20495d6e04284cb8592baac56fd7d955
Gromuf <luigi.beaumois@gmail.com>
2026-01-25 18:56:40 +0100

    fix: add console log for header initialization

--- Webpack/task_3/modules/header/header.js
+++ Webpack/task_3/modules/header/header.js
@@ -1,6 +1,8 @@
 const $ = require('jquery');
 require('./header.css');
 
+console.log('Init header');
+
 $(function() {
 	$('body').append('<div id="logo"></div>');
 	$('body').append('<h1>Holberton DDashboard</h1>');

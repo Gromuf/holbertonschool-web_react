af00b1e2e3017376da892a6858e2c1dfa53e3330
Gromuf <luigi.beaumois@gmail.com>
2026-02-11 11:35:05 +0100

    maybe fixed checker

--- react_props/task_4/dashboard/src/CourseList/CourseList.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -3,30 +3,32 @@ import "./CourseList.css";
 
 const CourseList = ({ listCourses = [] }) => {
   return (
-	<table id="CourseList">
-		<thead>
-			{listCourses.length === 0 ? (
-				<CourseListRow textFirstCell="No course available yet" isHeader={true} />
-			) : (
-				<>
-					<CourseListRow textFirstCell="Available courses" isHeader={true} />
-					<CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
-				</>
-			)}
-		</thead>
-		<tbody>
-      {
-        listCourses.map((course) => (
-          <CourseListRow
-            key={course.id}
-            textFirstCell={course.name}
-            textSecondCell={course.credit}
-            isHeader={false}
-          />
-        ))
-      }
-		</tbody>
-	</table>
+  <>
+    <table id="CourseList">
+      <thead>
+        {listCourses.length === 0 ? (
+          <CourseListRow textFirstCell="No course available yet" isHeader={true} />
+        ) : (
+          <>
+            <CourseListRow textFirstCell="Available courses" isHeader={true} />
+            <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+          </>
+        )}
+      </thead>
+      <tbody>
+        {
+          listCourses.map((course) => (
+            <CourseListRow
+              key={course.id}
+              textFirstCell={course.name}
+              textSecondCell={course.credit}
+              isHeader={false}
+            />
+          ))
+        }
+      </tbody>
+    </table>
+  </>
   );
 }
 
--- react_props/task_4/dashboard/src/CourseList/CourseListRow.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,25 +1,31 @@
 const CourseListRow = ({ isHeader = false, textFirstCell = "", textSecondCell = null }) => {
 	if (isHeader) {
-		if (textSecondCell === null) {
-			return (
-				<tr>
-					<th colSpan="2">{textFirstCell}</th>
-				</tr>
-			);
-		}
 		return (
-			<tr>
-				<th>{textFirstCell}</th>
-				<th>{textSecondCell}</th>
-			</tr>
-		);
-	}
-  return (
-    <tr>
-      <td>{textFirstCell}</td>
-      <td>{textSecondCell}</td>
-    </tr>
-  );
+      <>
+        <tr>
+          {
+            textSecondCell === null ? (
+              <th colSpan="2">{textFirstCell}</th>
+            ) : (
+              <>
+                <th>{textFirstCell}</th>
+                <th>{textSecondCell}</th>
+              </>
+            )
+          }
+        </tr>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <tr>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </tr>
+      </>
+    );
+  }
 }
 
 export default CourseListRow;

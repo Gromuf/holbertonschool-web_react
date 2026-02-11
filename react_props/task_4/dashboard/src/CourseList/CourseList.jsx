7ea43904117f83b7ad54aa3d98916a3b1beb0478
Gromuf <luigi.beaumois@gmail.com>
2026-02-11 11:28:40 +0100

    fix empty course list

--- react_props/task_4/dashboard/src/CourseList/CourseList.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -5,22 +5,26 @@ const CourseList = ({ listCourses = [] }) => {
   return (
 	<table id="CourseList">
 		<thead>
-			<CourseListRow textFirstCell="Available courses" isHeader={true} />
-			<CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+			{listCourses.length === 0 ? (
+				<CourseListRow textFirstCell="No course available yet" isHeader={true} />
+			) : (
+				<>
+					<CourseListRow textFirstCell="Available courses" isHeader={true} />
+					<CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+				</>
+			)}
 		</thead>
 		<tbody>
-		{listCourses.length === 0 ? (
-			<CourseListRow textFirstCell="No course available yet" isHeader={false} />
-		) : (
-			listCourses.map((course) => (
-				<CourseListRow
-					key={course.id}
-					textFirstCell={course.name}
-					textSecondCell={course.credit}
-					isHeader={false}
-				/>
-			))
-		)}
+      {
+        listCourses.map((course) => (
+          <CourseListRow
+            key={course.id}
+            textFirstCell={course.name}
+            textSecondCell={course.credit}
+            isHeader={false}
+          />
+        ))
+      }
 		</tbody>
 	</table>
   );

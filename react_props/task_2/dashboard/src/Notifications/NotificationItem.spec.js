8f32296a66517f21e384bd2bd80eda663a16ae97
Gromuf <luigi.beaumois@gmail.com>
2026-02-11 10:44:17 +0100

    essaie de test task 2 avec le test de sylvain

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -2,24 +2,17 @@ import { render, screen } from "@testing-library/react";
 import NotificationItem from "./NotificationItem.jsx";
 
 describe("NotificationItem component", () => {
-  test("renders blue li and correct data when type is default", () => {
-    render(<NotificationItem type="default" value="Test default" />);
-    const listItem = screen.getByRole("listitem");
-    expect(listItem).toHaveAttribute("data-notification-type", "default");
-    expect(listItem).toHaveStyle("color: blue");
-    expect(listItem).toHaveTextContent("Test default");
+  test("li has blue color and data-notification-type is default when type is default", () => {
+    render(<NotificationItem type="default" value="New course available" />);
+    const item = screen.getByRole("listitem");
+    expect(item).toHaveStyle({ color: "blue" });
+    expect(item).toHaveAttribute("data-notification-type", "default");
   });
-  test("renders red li and correct data when type is urgent", () => {
-    render(<NotificationItem type="urgent" value="Test urgent" />);
-    const listItem = screen.getByRole("listitem");
-    expect(listItem).toHaveAttribute("data-notification-type", "urgent");
-    expect(listItem).toHaveStyle("color: red");
-    expect(listItem).toHaveTextContent("Test urgent");
-  });
-  test("renders dangerouslySetInnerHTML when html prop is provided", () => {
-    const htmlContent = { __html: "<u>test html</u>" };
-    render(<NotificationItem type="default" html={htmlContent} />);
-    const listItem = screen.getByRole("listitem");
-    expect(listItem.innerHTML).toContain("<u>test html</u>");
+
+  test("li has red color and data-notification-type is urgent when type is urgent", () => {
+    render(<NotificationItem type="urgent" value="New resume available" />);
+    const item = screen.getByRole("listitem");
+    expect(item).toHaveStyle({ color: "red" });
+    expect(item).toHaveAttribute("data-notification-type", "urgent");
   });
 });

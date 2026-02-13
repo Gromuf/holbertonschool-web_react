101ed4f44dfd4b2394315b082461a013b35645f7
Gromuf <luigi.beaumois@gmail.com>
2026-02-13 11:05:24 +0100

    task 1

--- react_component/task_1/dashboard/src/App/App.spec.js
+++ react_component/task_1/dashboard/src/App/App.spec.js
@@ -1,5 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import App from "./App.jsx";
+
+afterEach(() => {
+  cleanup();
+});
 
 describe("App Component", () => {
   test("renders the h1 with text School dashboard", () => {

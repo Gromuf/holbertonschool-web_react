c4c349cc8af449277301385ea8e5fe54ee8b6c86
Gromuf <luigi.beaumois@gmail.com>
2026-02-17 15:40:22 +0100

    task 6

---
+++ react_component/task_5/dashboard/src/CourseList/CourseList.jsx
@@ -0,0 +1,35 @@
+import CourseListRow from "./CourseListRow";
+import "./CourseList.css";
+import WithLogging from '../HOC/WithLogging';
+
+const CourseList = ({ listCourses = [] }) => {
+  return (
+    <table id="CourseList">
+      {}
+      <thead>
+        {listCourses.length === 0 ? (
+          <CourseListRow textFirstCell="No course available yet" isHeader={true} />
+        ) : (
+          <>
+            <CourseListRow textFirstCell="Available courses" isHeader={true} />
+            <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+          </>
+        )}
+      </thead>
+      <tbody>
+        {listCourses.length > 0 &&
+          listCourses.map((course) => (
+            <CourseListRow
+              key={course.id}
+              textFirstCell={course.name}
+              textSecondCell={course.credit}
+              isHeader={false}
+            />
+          ))
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default WithLogging(CourseList);
---
+++ react_component/task_5/dashboard/src/Notifications/NotificationItem.spec.js
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import NotificationItem from "./NotificationItem.jsx";
+import "@testing-library/jest-dom";
+
+describe("NotificationItem component", () => {
+  test("li has blue color and data-notification-type is default when type is default", () => {
+    render(<NotificationItem type="default" value="New course available" />);
+    const item = screen.getByRole("listitem");
+    expect(item).toHaveStyle({ color: "blue" });
+    expect(item).toHaveAttribute("data-notification-type", "default");
+  });
+
+  test("li has red color and data-notification-type is urgent when type is urgent", () => {
+    render(<NotificationItem type="urgent" value="New resume available" />);
+    const item = screen.getByRole("listitem");
+    expect(item).toHaveStyle({ color: "red" });
+    expect(item).toHaveAttribute("data-notification-type", "urgent");
+  });
+  test("calls markAsRead prop with the right ID when clicked", () => {
+    const markAsReadMock = jest.fn();
+    const id = 7;
+    render(
+      <NotificationItem
+        id={id}
+        type="default"
+        value="test"
+        markAsRead={markAsReadMock}
+      />,
+    );
+    const item = screen.getByRole("listitem");
+    fireEvent.click(item);
+    expect(markAsReadMock).toHaveBeenCalledTimes(1);
+    expect(markAsReadMock).toHaveBeenCalledWith(id);
+  });
+});

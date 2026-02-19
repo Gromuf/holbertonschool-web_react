09e541f6589251f14dea4a598706ebceaa6d03a0
Gromuf <luigi.beaumois@gmail.com>
2026-02-19 15:18:30 +0100

    refactor: simplify CourseListRow component and update CSS variables

--- react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
@@ -2,36 +2,30 @@ import React from 'react';
 
 const CourseListRow = ({ isHeader = false, textFirstCell = "", textSecondCell = null }) => {
   const rowClasses = isHeader
-  ? "bg-table-header opacity-66"
-  : "bg-table-rows opacity-45";
+    ? "bg-table-header/66" 
+    : "bg-table-rows/45";
 
   const cellClasses = "border border-gray-400 pl-2 text-left";
 
-	if (isHeader) {
-		return (
-        <tr className={rowClasses}>
-          {
-            textSecondCell === null ? (
-              <th colSpan="2" className={cellClasses}>{textFirstCell}</th>
-            ) : (
-              <>
-                <th className={cellClasses}>{textFirstCell}</th>
-                <th className={cellClasses}>{textSecondCell}</th>
-              </>
-            )
-          }
-        </tr>
-    )
-  } else {
-    return (
-      <>
-        <tr className={rowClasses}>
+  return (
+    <tr className={rowClasses}>
+      {isHeader ? (
+        textSecondCell === null ? (
+          <th colSpan="2" className={cellClasses}>{textFirstCell}</th>
+        ) : (
+          <>
+            <th className={cellClasses}>{textFirstCell}</th>
+            <th className={cellClasses}>{textSecondCell}</th>
+          </>
+        )
+      ) : (
+        <>
           <td className={cellClasses}>{textFirstCell}</td>
           <td className={cellClasses}>{textSecondCell}</td>
-        </tr>
-      </>
-    );
-  }
-}
+        </>
+      )}
+    </tr>
+  );
+};
 
 export default CourseListRow;

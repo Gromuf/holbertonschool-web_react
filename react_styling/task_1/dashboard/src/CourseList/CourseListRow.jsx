96d6098cac8fea69ae2fbf22027be975ba71930c
Gromuf <luigi.beaumois@gmail.com>
2026-02-19 14:55:33 +0100

    task 1

---
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const CourseListRow = ({ isHeader = false, textFirstCell = "", textSecondCell = null }) => {
+  const rowClasses = isHeader
+  ? "bg-table-header opacity-66"
+  : "bg-table-rows opacity-45";
+
+  const cellClasses = "border border-gray-400 pl-2";
+
+	if (isHeader) {
+		return (
+      <>
+        <tr className={rowClasses}>
+          {
+            textSecondCell === null ? (
+              <th colSpan="2" className={cellClasses}>{textFirstCell}</th>
+            ) : (
+              <>
+                <th className={cellClasses}>{textFirstCell}</th>
+                <th className={cellClasses}>{textSecondCell}</th>
+              </>
+            )
+          }
+        </tr>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <tr className={rowClasses}>
+          <td className={cellClasses}>{textFirstCell}</td>
+          <td className={cellClasses}>{textSecondCell}</td>
+        </tr>
+      </>
+    );
+  }
+}
+
+export default CourseListRow;
---
+++ react_styling/task_1/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,80 @@
+import "./Notifications.css";
+import React, { Component } from "react";
+import closeIcon from "../assets/close-button.png";
+import NotificationItem from "./NotificationItem";
+
+class Notifications extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.notifications.length !== this.props.notifications.length;
+  }
+
+  handleClick = () => {
+    console.log("Close button has been clicked");
+  };
+
+  markAsRead = (id) => {
+    console.log(`Notification ${id} has been marked as read`);
+  };
+
+  static defaultProps = {
+    displayDrawer: false,
+    notifications: [],
+  };
+
+  render() {
+    const { notifications, displayDrawer } = this.props;
+    return (
+      <>
+        <div className="notification-title">Your notifications</div>
+        {displayDrawer ? (
+          <>
+            {notifications.length === 0 ? (
+              <>
+                <div className="notification-items">
+                  <p>No new notification for now</p>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="notification-items">
+                  <button
+                    aria-label="Close"
+                    style={{
+                      float: "right",
+                      background: "transparent",
+                      border: "none",
+                      cursor: "pointer",
+                    }}
+                    onClick={this.handleClick}
+                  >
+                    <img src={closeIcon} alt="close icon" />
+                  </button>
+                  <p>Here is the list of notifications</p>
+                  <ul>
+                    {notifications.map((notification) => {
+                      return (
+                        <NotificationItem
+                          key={notification.id}
+                          type={notification.type}
+                          value={notification.value}
+                          html={notification.html}
+                          markAsRead={() => this.markAsRead(notification.id)}
+                        />
+                      );
+                    })}
+                  </ul>
+                </div>
+              </>
+            )}
+          </>
+        ) : null}
+      </>
+    );
+  }
+}
+
+export default Notifications;

a92b1ff0ba7198a135507de462eb8957d17c6b9a
Gromuf <luigi.beaumois@gmail.com>
2026-02-20 10:59:59 +0100

    a

--- react_styling/task_4/dashboard/src/Notifications/Notifications.jsx
+++ react_styling/task_4/dashboard/src/Notifications/Notifications.jsx
@@ -20,32 +20,31 @@ class Notifications extends Component {
     const { notifications, displayDrawer } = this.props;
     return (
       <div className="flex flex-col items-end p-2 absolute right-0 top-0 z-50 max-[912px]:w-full max-[912px]:p-0">
-        {!displayDrawer && (
-          <div className="notification-title font-bold mb-2 max-[912px]:w-full max-[912px]:bg-white max-[912px]:text-right max-[912px]:pr-4 max-[912px]:m-0">
-            Your notifications
-          </div>
-        )}
+        <div className="notification-title font-bold mb-2 max-[912px]:w-full max-[912px]:bg-white max-[912px]:text-right max-[912px]:pr-4 max-[912px]:m-0 max-[912px]:p-2">
+          Your notifications
+        </div>
+
         {displayDrawer && (
           <div
             className="notification-items border-2 border-dashed border-main w-[25vw] p-2 relative bg-white 
-                          max-[912px]:fixed max-[912px]:inset-0 max-[912px]:w-full max-[912px]:h-full max-[912px]:border-none max-[912px]:text-[20px] max-[912px]:p-0"
+                          max-[912px]:fixed max-[912px]:inset-0 max-[912px]:w-full max-[912px]:h-full max-[912px]:border-none max-[912px]:text-[20px] max-[912px]:p-0 max-[912px]:z-50"
           >
             {notifications.length === 0 ? (
-              <p className="m-0 p-1.5 max-[912px]:p-3">
-                No new notification for now
-              </p>
+              <p className="m-0 p-3">No new notification for now</p>
             ) : (
-              <div className="max-[912px]:flex max-[912px]:flex-col">
+              <div className="flex flex-col">
                 <button
                   aria-label="Close"
-                  className="absolute right-2 top-2 bg-transparent border-none cursor-pointer outline-none z-10"
+                  className="absolute right-4 top-4 bg-transparent border-none cursor-pointer outline-none z-[60]"
                   onClick={() => console.log("Close button has been clicked")}
                 >
-                  <img src={closeIcon} alt="close icon" className="w-3 h-3" />
+                  <img src={closeIcon} alt="close icon" className="w-4 h-4" />
                 </button>
-                <p className="m-0 p-1.5 max-[912px]:p-3">
+
+                <p className="m-0 p-1.5 max-[912px]:p-3 max-[912px]:mt-10">
                   Here is the list of notifications
                 </p>
+
                 <ul className="list-disc pl-8 m-0 p-0 max-[912px]:list-none max-[912px]:p-0 max-[912px]:[&>li]:border-b max-[912px]:[&>li]:border-black max-[912px]:[&>li]:p-3">
                   {notifications.map((n) => (
                     <NotificationItem

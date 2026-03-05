2c0fd571cfa56ad2885472192b01da61e3c30be8
Gromuf <luigi.beaumois@gmail.com>
2026-03-05 11:18:11 +0100

    task 4

---
+++ react_hooks/task_3/dashboard/src/App/App.jsx
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import Notifications from "../Notifications/Notifications.jsx";
+import Header from "../Header/Header.jsx";
+import Login from "../Login/Login.jsx";
+import Footer from "../Footer/Footer.jsx";
+import CourseList from "../CourseList/CourseList.jsx";
+import { getLatestNotification } from "../utils/utils";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom.jsx";
+import BodySection from "../BodySection/BodySection.jsx";
+import AppContext, { user } from "../Context/context.js";
+
+const notifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+];
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false,
+      user: user,
+      listNotifications: notifications,
+      listCourses: listCourses,
+    };
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.logOut = this.logOut.bind(this);
+    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
+  }
+
+  markNotificationAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+    const updatedNotifications = this.state.listNotifications.filter(
+      (notification) => notification.id !== id
+    );
+    this.setState({ listNotifications: updatedNotifications });
+  }
+
+  logIn = (email, password) => {
+    this.setState({
+      user: {
+        email,
+        password,
+        isLoggedIn: true,
+      },
+    });
+  };
+
+  logOut() {
+    this.setState({
+      user: {
+        email: "",
+        password: "",
+        isLoggedIn: false,
+      },
+    });
+  }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  static defaultProps = {
+    isLoggedIn: false,
+    logOut: () => {},
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.logoutShortcutDetection);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.logoutShortcutDetection);
+  }
+
+  logoutShortcutDetection = (event) => {
+    if (event.ctrlKey && event.key === "h") {
+      alert("Logging you out");
+      this.logOut();
+    }
+  };
+
+  render() {
+    const { user, displayDrawer, listNotifications, listCourses } = this.state;
+    const value = { user, logOut: this.logOut };
+
+    return (
+      <AppContext.Provider value={value}>
+        <React.Fragment>
+          <div className="App flex flex-col min-h-screen">
+            <div className="root-notifications">
+              <Notifications
+                notifications={listNotifications}
+                displayDrawer={displayDrawer}
+                handleDisplayDrawer={this.handleDisplayDrawer}
+                handleHideDrawer={this.handleHideDrawer}
+                markNotificationAsRead={this.markNotificationAsRead}
+              />
+            </div>
+            <Header />
+            <div className="App-body flex-grow p-4 md:p-12">
+              {!user.isLoggedIn ? (
+                <BodySectionWithMarginBottom title="Log in to continue">
+                  <Login logIn={this.logIn} />
+                </BodySectionWithMarginBottom>
+              ) : (
+                <BodySectionWithMarginBottom title="Course list">
+                  <CourseList listCourses={listCourses} />
+                </BodySectionWithMarginBottom>
+              )}
+              <BodySection title="News from the School">
+                <p>Holberton School News goes here</p>
+              </BodySection>
+            </div>
+            <Footer isIndex={true} />
+          </div>
+        </React.Fragment>
+      </AppContext.Provider>
+    );
+  }
+}
+
+export default App;
---
+++ react_hooks/task_3/dashboard/src/Login/Login.spec.js
@@ -0,0 +1,68 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Login from "./Login.jsx";
+
+test("should render one button", () => {
+  render(<Login />);
+  expect(screen.getByText(/ok/i));
+});
+
+test("should verify that correct elements are rendered", () => {
+  render(<Login />);
+  const emailInput = screen.getByLabelText(/email/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const labels = screen.getAllByText(/email|password/i);
+  const button = screen.getByRole("button");
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+  expect(labels).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
+
+test("should focus on the 2 inputs when the related label is clicked", async () => {
+  render(<Login />);
+  const emailInput = screen.getByLabelText(/email/i);
+  const emailLabel = screen.getByText(/email/i);
+  await userEvent.click(emailLabel);
+  expect(emailInput).toHaveFocus();
+  const passwordInput = screen.getByLabelText(/password/i);
+  const passwordLabel = screen.getByText(/password/i);
+  await userEvent.click(passwordLabel);
+  expect(passwordInput).toHaveFocus();
+});
+
+describe("Login Component", () => {
+  test("submit button is disabled by default", () => {
+    render(<Login />);
+    const submitBtn = screen.getByRole("button", { name: /OK/i });
+    expect(submitBtn).toBeDisabled();
+  });
+
+  test("button becomes enabled after valid email and 8+ char password", () => {
+    render(<Login />);
+    const emailInput = screen.getByLabelText(/Email:/i);
+    const passwordInput = screen.getByLabelText(/Password:/i);
+    const submitBtn = screen.getByRole("button", { name: /OK/i });
+    fireEvent.change(emailInput, { target: { value: "user@test.com" } });
+    expect(submitBtn).toBeDisabled();
+    fireEvent.change(passwordInput, { target: { value: "12345" } });
+    expect(submitBtn).toBeDisabled();
+    fireEvent.change(passwordInput, { target: { value: "password123" } });
+    expect(submitBtn).toBeEnabled();
+  });
+
+  test("verify that the logIn method prop is correctly called with email and password", () => {
+    const logInMock = jest.fn();
+    render(<Login logIn={logInMock} />);
+    const emailInput = screen.getByLabelText(/Email:/i);
+    const passwordInput = screen.getByLabelText(/Password:/i);
+    const submitBtn = screen.getByRole("button", { name: /OK/i });
+    const testEmail = "test@holberton.com";
+    const testPassword = "superpassword123";
+    fireEvent.change(emailInput, { target: { value: testEmail } });
+    fireEvent.change(passwordInput, { target: { value: testPassword } });
+    fireEvent.click(submitBtn);
+    expect(logInMock).toHaveBeenCalledTimes(1);
+    expect(logInMock).toHaveBeenCalledWith(testEmail, testPassword);
+  });
+});

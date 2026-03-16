d5d56c5b63023dc8cb53e5a3817040e5abda2be4
Gromuf <luigi.beaumois@gmail.com>
2026-03-16 14:24:39 +0100

    task 2

---
+++ react_redux-part1/task_1/dashboard/src/features/notifications/notificationsSlice.js
@@ -0,0 +1,62 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { getLatestNotification } from "../../utils/utils";
+
+const initialState = {
+  notifications: [],
+  displayDrawer: true,
+};
+
+const API_BASE_URL = "http://localhost:5173";
+
+const ENDPOINTS = {
+  NOTIFICATIONS: `${API_BASE_URL}/notifications.json`,
+};
+
+export const fetchNotifications = createAsyncThunk(
+  "notifications/fetchNotifications",
+  async () => {
+    const response = await axios.get(ENDPOINTS.NOTIFICATIONS);
+    const data = response.data;
+    const updatedNotifications = data.map((notification) => {
+      if (notification.id === 3) {
+        return {
+          ...notification,
+          value: getLatestNotification(),
+        };
+      }
+      return notification;
+    });
+    return updatedNotifications;
+  },
+);
+
+const notificationsSlice = createSlice({
+  name: "notifications",
+  initialState,
+  reducers: {
+    markNotificationAsRead: (state, action) => {
+      const notificationId = action.payload;
+      console.log(`Log to console the removed notification id ${notificationId}`);
+      state.notifications = state.notifications.filter(
+        (notification) => notification.id !== notificationId,
+      );
+    },
+    showDrawer: (state) => {
+      state.displayDrawer = true;
+    },
+    hideDrawer: (state) => {
+      state.displayDrawer = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.notifications = action.payload;
+    });
+  },
+});
+
+export const { markNotificationAsRead, showDrawer, hideDrawer } =
+  notificationsSlice.actions;
+
+export default notificationsSlice.reducer;

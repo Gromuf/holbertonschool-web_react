a09537e9d1371c5f43e916650d33b015433e568f
Gromuf <luigi.beaumois@gmail.com>
2026-03-19 10:58:09 +0100

    task 2

--- react_redux-part2/task_2/dashboard/src/features/tests/coursesSlice.spec.js
+++ react_redux-part2/task_2/dashboard/src/features/tests/coursesSlice.spec.js
@@ -1,5 +1,9 @@
 import mockAxios from "jest-mock-axios";
-import coursesReducer, { fetchCourses } from "../courses/coursesSlice";
+import coursesReducer, {
+  fetchCourses,
+  selectCourse,
+  unselectCourse,
+} from "../courses/coursesSlice";
 import { logout } from "../auth/authSlice";
 
 describe("coursesSlice", () => {
@@ -18,24 +22,44 @@ describe("coursesSlice", () => {
   });
 
   test("should handle fetchCourses.fulfilled correctly", () => {
-    const mockCourses = [
+    const mockPayload = [
       { id: 1, name: "ES6 Basics", instructor: "John Doe" },
       { id: 2, name: "Redux Toolkit", instructor: "Jane Smith" },
     ];
+    const expectedState = [
+      { id: 1, name: "ES6 Basics", instructor: "John Doe", isSelected: false },
+      {
+        id: 2,
+        name: "Redux Toolkit",
+        instructor: "Jane Smith",
+        isSelected: false,
+      },
+    ];
     const action = {
       type: fetchCourses.fulfilled.type,
-      payload: mockCourses,
+      payload: mockPayload,
     };
     const newState = coursesReducer(initialState, action);
     expect(newState.courses).toHaveLength(2);
-    expect(newState.courses).toEqual(mockCourses);
+    expect(newState.courses).toEqual(expectedState);
+  });
+
+  test("should handle selectCourse and unselectCourse", () => {
+    const stateWithData = {
+      courses: [{ id: 1, name: "React", isSelected: false }],
+    };
+    let newState = coursesReducer(stateWithData, selectCourse(1));
+    expect(newState.courses[0].isSelected).toBe(true);
+    newState = coursesReducer(newState, unselectCourse(1));
+    expect(newState.courses[0].isSelected).toBe(false);
   });
 
   test("should reset the courses array to empty whenever the logout action is dispatched", () => {
     const stateWithData = {
-      courses: [{ id: 1, name: "React 101" }],
+      courses: [{ id: 1, name: "React 101", isSelected: false }],
     };
     const newState = coursesReducer(stateWithData, logout());
+
     expect(newState.courses).toHaveLength(0);
     expect(newState.courses).toEqual([]);
   });
